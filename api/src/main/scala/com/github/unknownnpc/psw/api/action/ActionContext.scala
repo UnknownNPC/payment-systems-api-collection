8be18dc2a0a6c3c0cd2e992a182a43e24755492b
Vitalii Zymukha <unknownvzzv@gmail.com>
2019-07-26 13:52:26 +0200

    Add minor checkstyle fixes

--- api/src/main/scala/com/github/unknownnpc/psw/api/action/ActionContext.scala
+++ api/src/main/scala/com/github/unknownnpc/psw/api/action/ActionContext.scala
@@ -6,7 +6,6 @@ import com.github.unknownnpc.psw.api.executor.Executor
 trait ActionContext[IN, OUT, REQ, RES] {
   executor: Executor[REQ, RES] =>
 
-
   def run(in: IN)(implicit ser: Serializer[IN, OUT, REQ, RES]): OUT = {
     ser.fromRes(
       executor.execute(
