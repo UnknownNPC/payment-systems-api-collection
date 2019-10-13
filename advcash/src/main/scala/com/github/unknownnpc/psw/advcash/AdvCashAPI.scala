e8eedd458743ba94c03a76ef133368da5357f43c
Vitalii Zymukha <unknownvzzv@gmail.com>
2019-10-13 21:23:03 +0300

    Add scaladoc for Advcash API

--- advcash/src/main/scala/com/github/unknownnpc/psw/advcash/AdvCashAPI.scala
+++ advcash/src/main/scala/com/github/unknownnpc/psw/advcash/AdvCashAPI.scala
@@ -17,6 +17,11 @@ private[advcash] class AdvCashAPI(apiName: String, apiPassword: String, apiEmail
   private val authZoneId = ZoneId.of("UTC")
   private val authTokenDateFormatter = DateTimeFormatter.ofPattern("yyyyMMdd:HH")
 
+  /**
+    * Get Balance per User’s Wallets.
+    *
+    * @return response with details or error
+    */
   def retrieveBalancePerWallets: Either[APIException, BalancesResponse] = {
     val request = AuthInfo(apiName, getAuthenticationToken(), apiEmail)
     retrieveBalancePerWalletsAction.run(request)
@@ -26,9 +31,22 @@ private[advcash] class AdvCashAPI(apiName: String, apiPassword: String, apiEmail
     retrieveBalancePerWallets
   }
 
+  /**
+    * Get Transaction History.
+    *
+    * @param from              Ordinal number of transaction to start displaying with
+    * @param count             The number of transactions for displaying
+    * @param sortOrder         Sorting (takes values «ASC», «DESC», default «DESC»)
+    * @param startTimeFrom     Start date for transactions to be selected
+    * @param startTimeTo       End date for transactions to be selected
+    * @param walletIdOpt       Wallet (optional parameter)
+    * @param transactionName   Transaction name
+    * @param transactionStatus Transaction status
+    * @return the response with transactions or error
+    */
   def retrieveTransactionsHistory(from: Int, count: Int, sortOrder: Sort.Value, startTimeFrom: Date, startTimeTo: Date, walletIdOpt: Option[String],
-                             transactionName: TransactionName.Value = TransactionName.ALL,
-                             transactionStatus: TransactionStatus.Value = TransactionStatus.COMPLETED): Either[APIException, TransactionHistoryResponse] = {
+                                  transactionName: TransactionName.Value = TransactionName.ALL,
+                                  transactionStatus: TransactionStatus.Value = TransactionStatus.COMPLETED): Either[APIException, TransactionHistoryResponse] = {
     val auth = AuthInfo(apiName, getAuthenticationToken(), apiEmail)
     val request = TransactionHistoryRequest(auth, from, count, sortOrder, startTimeFrom, startTimeTo, transactionName, transactionStatus, walletIdOpt)
 
@@ -36,7 +54,7 @@ private[advcash] class AdvCashAPI(apiName: String, apiPassword: String, apiEmail
   }
 
   def retrieveTransactionsHistoryJava(from: Int, count: Int, sortOrder: Sort.Value, startTimeFrom: Date, startTimeTo: Date,
-                                 walletIdOpt: Optional[String], transactionName: TransactionName.Value, transactionStatus: TransactionStatus.Value): Either[APIException, TransactionHistoryResponse] = {
+                                      walletIdOpt: Optional[String], transactionName: TransactionName.Value, transactionStatus: TransactionStatus.Value): Either[APIException, TransactionHistoryResponse] = {
     retrieveTransactionsHistory(
       from, count, sortOrder, startTimeFrom, startTimeTo, Option(walletIdOpt.orElse(null)), transactionName, transactionStatus
     )
